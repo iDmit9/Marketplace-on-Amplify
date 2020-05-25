019654f3bb6dd6fd9d9774e9de267479f5a354df
Dmitriy Ivanov <dmitriy.p86@ya.ru>
2020-05-25 23:12:00 +0600

    new product component finished & S3 Storage

--- src/components/NewProduct.js
+++ src/components/NewProduct.js
@@ -1,12 +1,175 @@
 import React from "react";
-// prettier-ignore
-// import { Form, Button, Input, Notification, Radio, Progress } from "element-react";
+import { Storage, Auth, API, graphqlOperation } from 'aws-amplify';
+import { createProduct } from '../graphql/mutations';
+import { PhotoPicker } from 'aws-amplify-react';
+import aws_exports from '../aws-exports';
+import { Form, Button, Input, Notification, Radio, Progress } from "element-react";
+import { convertDollarsToCents } from '../utils';
+
+const initialState = {
+  description: '',
+  price: '',
+  imagePreview: '',
+  image: '',
+  shipped: true,
+  isUploading: false,
+  percentUploaded: 0
+};
 
 class NewProduct extends React.Component {
-  state = {};
+  state = {
+    ...initialState
+  };
+
+  handleAddProduct = async () => {
+    try {
+      console.log(this.state)
+      this.setState({ isUploading: true });
+      const visibility = 'public';
+      const { identityId } = await Auth.currentCredentials();
+      const filename = `/${visibility}/${identityId}/${Date.now()}-${this.state.image.name}`
+      const uploadedFile = await Storage.put(filename, this.state.image.file, {
+        contentType: this.state.image.type,
+        progressCallback: progress => {
+          console.log(`Uploaded: ${progress.loaded}/${progress.total}`);
+          const percentUploaded = Math.round((progress.loaded / progress.total) * 100)
+          this.setState({ percentUploaded })
+        }
+      })
+      const file = {
+        key: uploadedFile.key,
+        bucket: aws_exports.aws_user_files_s3_bucket,
+        region: aws_exports.aws_user_files_s3_bucket_region
+      }
+      const input = {
+        productMarketId: this.props.marketId,
+        description: this.state.description,
+        shipped: this.state.shipped,
+        price: convertDollarsToCents(this.state.price),
+        file
+      }
+      const result = await API.graphql(graphqlOperation(createProduct, { input }));
+      console.log('Created product', result);
+      Notification({
+        title: "Success",
+        message: "Product successfully created",
+        type: 'success'
+      })
+      this.setState({ ...initialState });
+    } catch (err) {
+      console.error('Error adding product', err)
+    }
+  }
 
   render() {
-    return <div>NewProduct</div>
+    const {
+      description,
+      price,
+      image,
+      shipped,
+      imagePreview,
+      isUploading,
+      percentUploaded
+    } = this.state;
+
+    return (
+      <div className='flex-center'>
+        <h1 className="header">Add New Product</h1>
+        <div>
+          <Form className='market-header'>
+            <Form.Item label='Add Product Description'>
+              <Input
+                type='text'
+                icon='information'
+                placeholder='Description'
+                value={description}
+                onChange={description => this.setState({ description })}
+              />
+            </Form.Item>
+            <Form.Item label='Set Product Price'>
+              <Input
+                type='number'
+                icon='plus'
+                placeholder='Price ($USD)'
+                value={price}
+                onChange={price => this.setState({ price })}
+              />
+            </Form.Item>
+            <Form.Item label='Is the Product Shipped or Emailed'>
+              <div className="text-center">
+                <Radio
+                  value='true'
+                  checked={shipped === true}
+                  onChange={() => this.setState({ shipped: true })}
+                >
+                  Shipped
+                </Radio>
+                <Radio
+                  value='false'
+                  checked={shipped !== true}
+                  onChange={() => this.setState({ shipped: false })}
+                >
+                  Emailed
+                </Radio>
+              </div>
+            </Form.Item>
+            {imagePreview && (
+              <img
+                className='image-preview'
+                src={imagePreview}
+                alt='Product Preview'
+              />
+            )}
+            {percentUploaded > 0 && (
+              <Progress
+                type='circle'
+                className='progress'
+                percentage={percentUploaded}
+              />
+            )}
+            <PhotoPicker
+              title="Product Image"
+              preview='hidden'
+              onLoad={url => this.setState({ imagePreview: url })}
+              onPick={file => this.setState({ image: file })}
+              theme={{
+                formContainer: {
+                  margin: 0,
+                  padding: '0.8em'
+                },
+                formSection: {
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'center'
+                },
+                sectionBody: {
+                  margin: 0,
+                  width: '250px'
+                },
+                sectionHeader: {
+                  padding: '0.2em',
+                  color: 'var(--darkAmazonOrange)'
+                },
+                photoPickerButton: {
+                  display: 'none'
+                }
+              }}
+            />
+            <Form.Item>
+              <Button
+                disabled={!image || !description || !price || isUploading}
+                type='primary'
+                onClick={this.handleAddProduct}
+                loading={isUploading}
+              >
+                {isUploading ? 'Uploading...' : 'Add Product'}
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
+    )
   }
 }
 
